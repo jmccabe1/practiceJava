f068e73dc8b9b3c8a29773bc82d9d06abd23b185
JohnMcCabe <fredthefurrypotato@hotmail.com>
2021-05-20 18:15:04 -0400

    longest substring between unique characters puzzle attempt 2 (tested and passed)

--- src/main/java/com/example/NewPassword/LongestUniqueSubstring.java
+++ src/main/java/com/example/NewPassword/LongestUniqueSubstring.java
@@ -17,14 +17,14 @@ public class LongestUniqueSubstring {
         List<Character> bookends = new ArrayList<>();//This is where we'll store the unique characters
         for(int x = 0; x < input.length(); x++) { //We loop through each character in the input to test out each character
             for (int i = 0; i < input.length(); i++) {//We're looping through them all again, this time to get the second comparitor
-                if ( input.charAt(x) == input.charAt(i) || x != i) { //If the one we're comparing to is the same as the one we're comparing from and isn't at the same position marking it as the same character as our tested character,
+                if ( input.charAt(x) == input.charAt(i) && x != i) { //If the one we're comparing to is the same as the one we're comparing from and isn't at the same position marking it as the same character as our tested character,
                     break;//...we know we have a character that isn't unique and we can stop testing this character. This break should get us out of the first "i" for loop without getting out of the larger "x" for loop
                 } else if (i + 1 >= input.length()) {//If we've reached the end of arr and the next iteration of the for loop with end testing this char, and we still haven't found a duplicate, we have a unique char
                     bookends.add(input.charAt(x));//So, we add it to the list of unique characters we created on line 18
                 }
             }
         }
-        solution = input.substring(input.indexOf(bookends.get(0)), input.indexOf(bookends.get(bookends.size() - 1)));
+        solution = input.substring(input.indexOf(bookends.get(0)) + 1, input.indexOf(bookends.get(bookends.size() - 1)));
         //This long line finds the substring we want in input by grabbing the first and last chars in bookends and finding the index of those
         //chars in input, then using those as the substring's beginning and end indexes to create the desired substring and save it in solution
         return solution;
--- src/test/java/com/example/NewPassword/LongestUniqueSubstringTest.java
+++ src/test/java/com/example/NewPassword/LongestUniqueSubstringTest.java
@@ -8,15 +8,18 @@ import org.springframework.util.Assert;
 public class LongestUniqueSubstringTest {
     @Test
     void substringTestOne() {
-        Assert.isTrue(LongestUniqueSubstring.findSubstring("accbaaaccaccadaac") == "aaaccacca");
+        Assert.isTrue(LongestUniqueSubstring.findSubstring("accbaaaccaccadaac").equals("aaaccacca"));
         //Personal note: depending on whether substring() is inclusive of its parameter indexes or not, this might include B and D in the
         //returned substring. This can be fixed fairly easily, I believe, though it will likely require splitting up the logic into
         //multiple lines that's currently completely enclosed in LongestUniqueSubstring line 27
+
+        //Update: it was inclusive but D was already fixed, added similar logic to B side so it works without splitting lines now.
+        //Also, I need to remember to use .equals() instead of == in tests.
     }
 
     @Test
     void substringTestTwo() {
-        Assert.isTrue(LongestUniqueSubstring.findSubstring("accbaaaccvaccadaac") == "aaaccvacca");
+        Assert.isTrue(LongestUniqueSubstring.findSubstring("accbaaaccvaccadaac").equals("aaaccvacca"));
         //In all honesty, including V in the returned substring even though V is unique doesn't feel right, but if that's
         //what the question is asking for, who am I to judge
     }
